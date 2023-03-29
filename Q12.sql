f4a38b628f5f8fe0dd5da23e6f989f34aacacdc8
Muhammad Abdur Rafey <115175167+MARafey@users.noreply.github.com>
2023-03-29 13:08:53 +0500

    Update Q12.sql

--- Q12.sql
+++ Q12.sql
@@ -1,10 +1,6 @@
-SELECT O.o_name
-FROM owners O
-JOIN OWNS OW ON O.SSN = OW.SSN
-JOIN AirPlane A ON OW.Reg = A.RegNo
+SELECT PT.ModelNo, AVG(S.Hours) AS AvgMaintenanceHours
+FROM Plane_Type PT
+JOIN AirPlane A ON PT.ModelNo = A.OF_TYPE
 JOIN PLANE_SERVICE PS ON A.RegNo = PS.Reg
 JOIN SERVICE S ON PS.ServiceID = S.ServiceID
-JOIN MAINTAIN M ON S.ServiceID = M.ServiceID
-JOIN Employee E ON M.EmployeeID = E.EmployeeID
-LEFT JOIN WORKS_ON WO ON E.EmployeeID = WO.EmployeeID AND A.OF_TYPE = WO.Model
-WHERE WO.EmployeeID IS NULL;
+GROUP BY PT.ModelNo;
